cda78c18082fe1a52f04389645dd12497c152c47
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2022-06-09 14:21:04 -0300

    new: roll functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  roll = 0;
+
+  constructor(public alertController: AlertController) {
+  }
+
+  async presentAlert(rollValue) {
+    rollValue += 1;
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Roll',
+      message: rollValue,
+      buttons: [
+        {
+          text: 'RODAR NOVAMENTE',
+          role: 'rodar',
+          cssClass: 'secondary',
+          id: 'cancel-button',
+          handler: () => {
+            this.rollDice();
+          }
+        }, 'OK'
+    ]
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  rollDice(){
+    return this.presentAlert(Math.floor(Math.random()*20));
+  }
 
 }
