43ae312a0c2367769b1bc42dd9936be9fbc1ca3d
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-06-25 23:50:33 -0300

    new: added dice 3D view-model + dice functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,38 +9,124 @@ import { AlertController } from '@ionic/angular';
 
 export class HomePage {
 
-  roll = 0;
+  result = 0;
 
-  constructor(public alertController: AlertController) {
+  x = 90;
+  y = 90;
+
+  loadedDice = false;
+  diceElement;
+
+  constructor(public toastController: ToastController) {
   }
 
-  async presentAlert(rollValue) {
-    rollValue += 1;
-    const alert = await this.alertController.create({
-      cssClass: 'resultado',
-      header: 'RESULTADO',
-      message: rollValue,
-      buttons: [
-        {
-          text: 'RODAR NOVAMENTE',
-          role: 'rodar',
-          cssClass: 'resultado',
-          id: 'roll-button',
-          handler: () => {
-            this.rollDice();
-          }
-        }, 'OK'
-    ]
+  async showNotification(result: number) {
+    const toast = await this.toastController.create({
+      icon: 'Dice',
+      message: `RESULTADO FINAL: ${result}`,
+      duration: 2000
     });
-
-    await alert.present();
-
-    const { role } = await alert.onDidDismiss();
-    console.log('onDidDismiss resolved with role', role);
+    toast.present();
   }
 
-  rollDice(){
-    return this.presentAlert(Math.floor(Math.random()*20));
+  delay(delayInms) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(2);
+      }, delayInms);
+    });
+  }
+
+  getRandomNumberInRange(maxLimit: number = 180) {
+    return Math.floor(
+      Math.random() * maxLimit
+    ) + 1;
+  }
+
+  getDiceCoordinate() {
+    const id = this.getRandomNumberInRange(4);
+    if (id === 1) {
+      return -90;
+    } else {
+      if (id === 2) {
+        return 90;
+      } else {
+        if (id === 3) {
+          return -90;
+        }
+      }
+    }
+    return 90;
+  }
+
+  getResult(): number {
+    if (this.y === 0 || this.y === -90) {
+      return 1;
+    }
+    if (this.x === 90 && this.y === 90) {
+      return 2;
+    }
+    if (this.x === 180 && this.y === 90) {
+      return 3;
+    }
+    if (this.x === -90 && this.y === 90) {
+      return 5;
+    }
+    if (this.x === 0 && this.y === 90) {
+      return 4;
+    }
+    if (this.y === 180) {
+      return 6;
+    }
+    return 0;
+  }
+
+  limitValue(num: number): number{
+    const MIN = -90;
+    const MAX = 180;
+    return Math.min(Math.max(num, MIN), MAX);
+  }
+
+  setAngle(z, desiredValue) {
+    z = this.limitValue(desiredValue);
+  }
+
+  async rollDice() {
+    if (this.loadedDice === false) {
+      this.loadDice();
+    }
+    for (let i = 0; i < 5; i ++) {
+
+      const xResult = this.getDiceCoordinate();
+      const yResult = this.getDiceCoordinate();
+
+      const currentY = this.y;
+      const currentX = this.x;
+
+      this.x = this.limitValue(currentX + xResult);
+      this.y = this.limitValue(currentY + yResult);
+
+      if (this.x === currentX) {
+        this.x = this.limitValue(xResult);
+      }
+
+      if (this.y === currentY) {
+        this.y = this.limitValue(yResult);
+      }
+
+      this.result = this.getResult();
+
+      this.diceElement.setAttribute('camera-orbit', `${this.x}deg ${this.y}deg`);
+      await this.delay(500);
+
+    }
+
+    this.showNotification(this.getResult());
+  }
+
+  loadDice() {
+    console.log('Loaded dice');
+    this.diceElement = document.querySelector('.viewer');
   }
 
 }
